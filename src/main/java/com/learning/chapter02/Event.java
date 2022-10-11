d95d190ecf9afdcfe5c0609bd86cfe32448bb061
方得着 <kinghtdom@outlook.com>
2022-10-11 20:36:31 +0800

    源算子

---
+++ src/main/java/com/learning/chapter02/Event.java
@@ -0,0 +1,33 @@
+package com.learning.chapter02;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Flink - Event
+ *
+ * @Author: Edgar Fang
+ * @Date: 2022/9/25 16:51
+ */
+public class Event {
+    public String user;
+    public String url;
+    public Long timestamp;
+
+    public Event() {
+    }
+
+    public Event(String user, String url, Long timestamp) {
+        this.user = user;
+        this.url = url;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "user='" + user + '\'' +
+                ", url='" + url + '\'' +
+                ", timestamp=" + new Timestamp(timestamp) +
+                '}';
+    }
+}
---
+++ src/main/java/com/learning/chapter02/SourceTest.java
@@ -0,0 +1,47 @@
+package com.learning.chapter02;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Flink - SourceTest
+ *
+ * @Author: Edgar Fang
+ * @Date: 2022/9/25 16:59
+ */
+public class SourceTest {
+    public static void main(String[] args) throws Exception{
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 1 从文件中读取数据
+        DataStreamSource<String> stream1 = env.readTextFile("input/clicks.txt");
+
+        // 2.1 从集合中读取数据
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(2);
+        nums.add(5);
+        DataStreamSource<Integer> numStream = env.fromCollection(nums);
+
+        // 2.2 定义一个Event类型
+        ArrayList<Event> events = new ArrayList<>();
+        events.add(new Event("Mary", "./home", 1000L));
+        events.add(new Event("Bob", "./cart", 2000L));
+        DataStreamSource<Event> stream2 = env.fromCollection(events);
+
+        // 2.3 从元素读取数据
+        DataStreamSource<Event> stream3 = env.fromElements(
+                new Event("Mary", "./home", 1000L),
+                new Event("Bob", "./cart", 2000L)
+        )
+
+        stream1.print("1");
+        numStream.print("nums");
+        stream2.print("2");
+
+        env.execute();
+    }
+}
