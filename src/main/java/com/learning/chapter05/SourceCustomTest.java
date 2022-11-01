9791ccdbabc9ee26aea52375fddedafe5726db53
方得着 <kinghtdom@outlook.com>
2022-11-01 23:12:19 +0800

    定义自定义数据源和并行自定义数据源

--- src/main/java/com/learning/chapter05/SourceCustomTest.java
+++ src/main/java/com/learning/chapter05/SourceCustomTest.java
@@ -2,6 +2,9 @@ package com.learning.chapter05;
 
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+
+import java.util.Random;
 
 /**
  * Created by Flink - SourceCustomTest
@@ -14,10 +17,32 @@ public class SourceCustomTest {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        DataStreamSource<Event> customStream = env.addSource(new ClickSource());
+        // 获取自定义数据源
+//        DataStreamSource<Event> customStream = env.addSource(new ClickSource());
+
+        // 获取并行的自定义数据源
+        DataStreamSource<Integer> customStream = env.addSource(new ParallelCustomSource()).setParallelism(4);
 
         customStream.print();
 
         env.execute();
+    }
+
+    public static class ParallelCustomSource implements ParallelSourceFunction<Integer> {
+        private Boolean running = true;
+        private Random random = new Random();
+
+        @Override
+        public void run(SourceContext<Integer> ctx) throws Exception {
+            while (running) {
+                ctx.collect(random.nextInt());
+                Thread.sleep(1000L);
+            }
+        }
+
+        @Override
+        public void cancel() {
+            running = false;
+        }
     }
 }
