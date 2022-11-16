aa8b8a265e1aa5061b9c71b5792bbeb9904c94c7
方得着 <kinghtdom@outlook.com>
2022-11-16 22:40:25 +0800

    全窗口函数

--- src/main/java/com/learning/chapter06/WindowProcessTest.java
+++ src/main/java/com/learning/chapter06/WindowProcessTest.java
@@ -4,24 +4,25 @@ import com.learning.chapter05.ClickSource;
 import com.learning.chapter05.Event;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.AggregateFunction;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
+import java.sql.Timestamp;
 import java.time.Duration;
-import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * Created by Flink - WindowAggregateTest_PvUv
+ * Created by Flink - WindowProcessTest
  *
  * @Author: Edgar Fang
- * @Date: 2022/11/15 23:37
+ * @Date: 2022/11/16 22:23
  */
-public class WindowAggregateTest_PvUv {
+public class WindowProcessTest {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
@@ -39,35 +40,31 @@ public class WindowAggregateTest_PvUv {
 
         stream.print("data");
 
+        //使用ProcessWindowFunction计算UV
         stream.keyBy(data -> true)
-                .window(SlidingEventTimeWindows.of(Time.seconds(10), Time.seconds(2)))
-                .aggregate(new AvgPv())
+                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+                .process(new UvCountByWindow())
                 .print();
 
         env.execute();
     }
 
-    public static class AvgPv implements AggregateFunction<Event, Tuple2<Long, HashSet<String>>, Double> {
+    public static class UvCountByWindow extends ProcessWindowFunction<Event, String, Boolean, TimeWindow> {
 
         @Override
-        public Tuple2<Long, HashSet<String>> createAccumulator() {
-            return Tuple2.of(0L, new HashSet<>());
-        }
+        public void process(Boolean aBoolean, Context context, Iterable<Event> elements, Collector<String> out) throws Exception {
+            HashSet<String> userSet = new HashSet<>();
+            // 从elements中遍历数据，放到set中去重
+            for (Event event: elements) {
+                userSet.add(event.user);
+            }
 
-        @Override
-        public Tuple2<Long, HashSet<String>> add(Event value, Tuple2<Long, HashSet<String>> accumulator) {
-            accumulator.f1.add(value.user);
-            return Tuple2.of(accumulator.f0 + 1, accumulator.f1);
-        }
+            int uv = userSet.size();
+            // 获取窗口信息
+            long start = context.window().getStart();
+            long end = context.window().getEnd();
+            out.collect("窗口：" + new Timestamp(start) + " ~ " + new Timestamp(end) + " UV值为：" + uv);
 
-        @Override
-        public Double getResult(Tuple2<Long, HashSet<String>> accumulator) {
-            return (double)accumulator.f0 / accumulator.f1.size();
-        }
-
-        @Override
-        public Tuple2<Long, HashSet<String>> merge(Tuple2<Long, HashSet<String>> a, Tuple2<Long, HashSet<String>> b) {
-            return null;
         }
     }
 }
