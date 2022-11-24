14245e4f1015568182a4c4249e0bbd3e1adf365e
方得着 <kinghtdom@outlook.com>
2022-11-24 17:26:26 +0800

    分流、合流操作

---
+++ src/main/java/com/learning/chapter08/UnionTest.java
@@ -0,0 +1,66 @@
+package com.learning.chapter08;
+
+import com.learning.chapter05.Event;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+
+/**
+ * Created by Flink - UnionTest
+ *
+ * @author Edgar Fang
+ * @date 2022/11/22 16:54
+ */
+public class UnionTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        SingleOutputStreamOperator<Event> stream1 = env.socketTextStream("localhost", 7777)
+                .map(data -> {
+                    String[] field = data.split(",");
+                    return new Event(field[0].trim(), field[1].trim(), Long.valueOf(field[2].trim()));
+                })
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event event, long l) {
+                                return event.timestamp;
+                            }
+                        }));
+
+        stream1.print("stream1");
+
+        SingleOutputStreamOperator<Event> stream2 = env.socketTextStream("localhost", 8888)
+                .map(data -> {
+                    String[] field = data.split(",");
+                    return new Event(field[0].trim(), field[1].trim(), Long.valueOf(field[2].trim()));
+                })
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event event, long l) {
+                                return event.timestamp;
+                            }
+                        }));
+
+        stream1.print("stream2");
+
+        //  合并2条数据流 union合并，获得两条流中的小的水位线，且两条流中的数据类型必须相同
+        stream1.union(stream2)
+                .process(new ProcessFunction<Event, String>() {
+                    @Override
+                    public void processElement(Event value, Context ctx, Collector<String> out) throws Exception {
+                        out.collect("水位线：" + ctx.timerService().currentWatermark());
+                    }
+                })
+                .print();
+    }
+}
