67a735a040707fa832bd31c53a45ed9f1b1003bd
方得着 <kinghtdom@outlook.com>
2023-01-09 23:39:53 +0800

    cep定义模式匹配

---
+++ src/main/java/com/learning/chapter12/LoginEvent.java
@@ -0,0 +1,34 @@
+package com.learning.chapter12;
+
+/**
+ * Created by Flink - LoginEvent
+ *
+ * @Author: Edgar Fang
+ * @Date: 2023/1/4 21:17
+ */
+public class LoginEvent {
+    public String userId;
+    public String ipAddress;
+    public String eventType;
+    public Long timestamp;
+
+    public LoginEvent() {
+    }
+
+    public LoginEvent(String userId, String ipAddress, String eventType, Long timestamp) {
+        this.userId = userId;
+        this.ipAddress = ipAddress;
+        this.eventType = eventType;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginEvent{" +
+                "userId='" + userId + '\'' +
+                ", ipAddress='" + ipAddress + '\'' +
+                ", eventType='" + eventType + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
